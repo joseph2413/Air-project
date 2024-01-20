8be7a6d4bcd06c22575d4ee6b82745e25c8c2fb4
Joseph Sellers <sellers.joseph04@gmail.com>
2024-01-20 11:35:34 -0800

    finished ph5

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -38,6 +38,8 @@ const validateSignup = [
 
       const safeUser = {
         id: user.id,
+        firstname: user.firstname,
+        lastname: user.lastname,
         email: user.email,
         username: user.username,
       };
