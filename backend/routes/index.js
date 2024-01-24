ccf24a930f9d70cce555e8a8f90f5829d7aa870a
Joseph Sellers <sellers.joseph04@gmail.com>
2024-01-23 22:13:02 -0800

    working on spots table

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -10,7 +10,7 @@ router.use('/api', apiRouter)
 
 router.get('/api/csrf/restore', (req, res) =>{
     const csrfToken = req.csrfToken();
-    res.cookie('XRSF-TOKEN', csrfToken);
+    res.cookie('XSRF-TOKEN', csrfToken);
     res.status(200).json({
         'XSRF-TOKEN': csrfToken
     });
