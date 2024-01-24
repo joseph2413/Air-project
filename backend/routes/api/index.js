c45ceea25fc6746db3f2504a645f9a2bac378ac0
Joseph Sellers <sellers.joseph04@gmail.com>
2024-01-23 22:25:58 -0800

    fixed migration errors

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -20,35 +20,3 @@ router.post('/test', function(req, res) {
 
 
 module.exports = router;
-
-
-// const { setTokenCookie } = require('../../utils/auth.js');
-// const { User } = require('../../db/models');
-// router.get('/set-token-cookie', async (_req, res) => {
-//   const user = await User.findOne({
-//     where: {
-//       username: 'DemoU'
-//     }
-//   });
-//   setTokenCookie(res, user);
-//   return res.json({ user: user });
-// });
-// const { restoreUser } = require('../../utils/auth.js');
-
-// router.use(restoreUser);
-
-// router.get(
-//   '/restore-user',
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
-
-// const { requireAuth } = require('../../utils/auth.js');
-// router.get(
-//   '/require-auth',
-//   requireAuth,
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
