37967af444566785cac7a824e5eb147a80562116
Joseph Sellers <sellers.joseph04@gmail.com>
2024-01-26 13:22:20 -0800

    fixing bugs in spot, user and spotImage tables

--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -1,42 +1,38 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class SpotImage extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      SpotImage.belongsTo(
-        models.Spot,{
-          foreignKey: 'spotId'
-        }
-      )
-    }
-  }
-  SpotImage.init({
-    spotId:{
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    url: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    preview: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-    },
-  }, {
-    sequelize,
-    modelName: 'SpotImage',
-    defaultScope: {
-      attributes: ["id", "url", "preview"],
-    }
-  });
-  return SpotImage;
+	class SpotImage extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			SpotImage.belongsTo(models.Spot, { foreignKey: "spotId" });
+		}
+	}
+	SpotImage.init(
+		{
+			spotId: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
+			url: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			preview: {
+				type: DataTypes.BOOLEAN,
+				allowNull: false,
+			},
+		},
+		{
+			sequelize,
+			modelName: "SpotImage",
+			defaultScope: {
+				attributes: ["id", "url", "preview"],
+			},
+		},
+	);
+	return SpotImage;
 };
