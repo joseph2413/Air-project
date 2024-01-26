912df6300b543fc94936d89b68ce4b8455e52dc3
Joseph Sellers <sellers.joseph04@gmail.com>
2024-01-25 20:01:29 -0800

    issues with associations

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -11,22 +11,13 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-			// Spot.hasMany(models.Booking, {
-			// 	foreignKey: "spotId",
-			// 	onDelete: "CASCADE",
-			// 	hooks: true,
-			// });
 			Spot.hasMany(models.SpotImage, {
 				foreignKey: "spotId",
 				onDelete: "CASCADE",
 				hooks: true,
 			});
-			// Spot.hasMany(models.Review, {
-			// 	foreignKey: "spotId",
-			// 	onDelete: "CASCADE",
-			// 	hooks: true,
-			// });
-			Spot.belongsTo(models.User, {as: "Owner", foreignKey: "ownerId" });
+
+			Spot.belongsTo(models.User, {foreignKey: "ownerId" });
 		}
   }
   Spot.init({
